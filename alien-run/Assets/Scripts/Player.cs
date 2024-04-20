18eca6b0dfb56301877fa6d5da72536703423112
mlogic1 <multilogic@outlook.com>
2024-04-20 21:41:14 +0200

    added basic player animations (idle, move and jump)

--- alien-run/Assets/Scripts/Player.cs
+++ alien-run/Assets/Scripts/Player.cs
@@ -11,12 +11,16 @@ public class Player : MonoBehaviour
 	public float JumpStrength = 0.25f;
 
 	private Rigidbody2D m_body;
+	private Animator m_animator;
 	private bool m_isGrounded = false;
+	private float m_horizontalInput = .0f;
+
 
 
 	private void Awake()
 	{
 		m_body = GetComponent<Rigidbody2D>();
+		m_animator = GetComponent<Animator>();
 	}
 
 	void Start()
@@ -31,15 +35,16 @@ public class Player : MonoBehaviour
 			return;
 		}
 
-		string debugText = "Horizontal input: " + Input.GetAxis("Horizontal").ToString() + "\r\n";
+		string debugText = "Horizontal input: " + m_horizontalInput.ToString() + "\r\n";
 		debugText += "Is Gounded: " + m_isGrounded;
 		DebugTextComponent.SetText(debugText);
 	}
 
 	void Update()
 	{
+		m_horizontalInput = Input.GetAxis("Horizontal");
 		WriteDebugData();
-		m_body.velocity = new Vector2(Input.GetAxis("Horizontal") * Velocity, m_body.velocity.y);
+		m_body.velocity = new Vector2(m_horizontalInput * Velocity, m_body.velocity.y);
 
 		if (Input.GetKey(KeyCode.Space) && m_isGrounded)// || Input.GetButton("JUMP"))
 		{
@@ -47,6 +52,29 @@ public class Player : MonoBehaviour
 			m_body.velocity += JumpStrength * Vector2.up;
 			m_isGrounded = false;
 		}
+
+		// flip character left or right
+		if (m_horizontalInput > 0.0f)
+		{
+			transform.localScale = Vector3.one;
+		}
+		if (m_horizontalInput < 0.0f)
+		{
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		UpdateAnimator();
+	}
+
+	private void UpdateAnimator()
+	{
+		if (m_isGrounded)
+		{
+			// player is either idle or moving
+			m_animator.SetBool("IsMoving", !Mathf.Approximately(m_horizontalInput, 0.0f));
+		}
+
+		m_animator.SetBool("IsGrounded", m_isGrounded); // player is in the air
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
